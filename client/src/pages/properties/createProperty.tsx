181a40d193e9615fa943bcd556ea544f99a63759
Genilton Souza <genilton.souza26@gmail.com>
2023-04-17 21:47:55 -0400

    feat(property): add create page

--- client/src/pages/properties/createProperty.tsx
+++ client/src/pages/properties/createProperty.tsx
@@ -1,5 +1,54 @@
 import React from "react";
+import { useState } from "react";
+import { useGetIdentity } from "@refinedev/core";
+//import { FieldValues, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { Form } from "components";
+import { useForm } from "@refinedev/react-hook-form";
+import { FieldValues } from "react-hook-form";
 
 export const CreateProperty = () => {
-  return <div>createProperty</div>;
+  const navigate = useNavigate();
+  const { data: user } = useGetIdentity<{
+    id: number;
+    email: string;
+  }>();
+  const [propertyImage, setPropertyImage] = useState({ name: "", url: "" });
+  const {
+    refineCore: { onFinish, formLoading },
+    register,
+    handleSubmit,
+  } = useForm();
+
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) =>
+      new Promise<string>((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.onload = () => resolve(fileReader.result as string);
+        fileReader.readAsDataURL(readFile);
+      });
+
+    reader(file).then((result: string) =>
+      setPropertyImage({ name: file?.name, url: result })
+    );
+  };
+
+  const onFinishHandler = async (data: FieldValues) => {
+    if (!propertyImage.name) return alert("Please select an image");
+
+    await onFinish({ ...data, photo: propertyImage.url, email: user?.email });
+  };
+
+  return (
+    <Form
+      type="Create"
+      register={register}
+      onFinish={onFinish}
+      formLoading={formLoading}
+      handleSubmit={handleSubmit}
+      handleImageChange={handleImageChange}
+      onFinishHandler={onFinishHandler}
+      propertyImage={propertyImage}
+    />
+  );
 };
